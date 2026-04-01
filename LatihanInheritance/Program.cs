0c8d527036369621b8416fd60331fe6c013b47e0
bismokodono <bismokodono@gmail.com>
2026-04-01 09:13:15 +0700

    membuat class induk user

--- LatihanInheritance/Program.cs
+++ LatihanInheritance/Program.cs
@@ -16,6 +16,37 @@ public class User
 
     public void TampilkanInfo()
     {
-        Console.WriteLine($"Nama:  {nama} | Role ");
+        Console.WriteLine($"Nama: {nama} | Role: {role}");
     }
 }
+class Siswa : User
+{
+    public string className;
+
+    public Siswa(string nama, string email, string password, string role, string className) : base(nama, email, password, role)
+    {
+        className = className;
+    }
+
+    public void SubmitTugas()
+    {
+        Console.WriteLine($"{nama} dari kelas {className} sudah mengumpulkan tugas");
+    }
+
+}
+
+class Guru : User
+{
+    public string mapel;
+
+    public Guru(string nama, string email, string password, string role, string mapel) : base(nama, email, password, role)
+    {
+        mapel = mapel;
+    }
+
+    public void NilaiTugas()
+    {
+        Console.WriteLine($"Guru {nama} telah manilai semua tugas {mapel}");
+    }
+}
+
